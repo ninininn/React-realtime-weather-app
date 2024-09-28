103417ab2adddc50e76991865b39f29aca18f715
niniku <niniku1457@gmail.com>
2024-09-28 17:11:11 +0800

    fetch when-click-reload(useState)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 //透過import方式把CSS或其他JS檔載入
-import React from "react";
+//從react載入useState,useEffect方法
+import React, { useState } from "react";
+
 //用react-create-app提拱的ReactComponent元件引入svg圖示
 import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
 import { ReactComponent as RainIcon } from "./images/rain.svg";
@@ -9,10 +11,11 @@ import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
 //移除引用App.css，載入emotion
 // import "./App.css";
 import styled from "@emotion/styled";
-
+//引用ThemeProvider
+import { ThemeProvider } from "@emotion/react";
 //元件式寫法CSS
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -22,20 +25,21 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
   box-sizing: border-box;
   padding: 30px 15px;
 `;
 
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `;
 
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `;
 
@@ -47,7 +51,7 @@ const CurrentWeather = styled.div`
 `;
 
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -63,7 +67,7 @@ const AirFlow = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
   svg {
     width: 25px;
@@ -77,7 +81,7 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   svg {
     width: 25px;
     height: auto;
@@ -93,7 +97,7 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   svg {
     width: 15px;
     height: 15px;
@@ -108,33 +112,112 @@ const DayCloudy = styled(DayCloudyIcon)`
   flex-basis: 30%;
 `;
 
+//定義主題色搭配
+const theme = {
+  light: {
+    backgroundColor: "#ededed",
+    foregroundColor: "#f9f9f9",
+    boxShadow: "0 1px 3px 0 #999999",
+    titleColor: "#212121",
+    temperatureColor: "#757575",
+    textColor: "#828282",
+  },
+  dark: {
+    backgroundColor: "#1F2022",
+    foregroundColor: "#121416",
+    boxShadow:
+      "0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)",
+    titleColor: "#f9f9fa",
+    temperatureColor: "#dddddd",
+    textColor: "#cccccc",
+  },
+};
+//將自己的授權碼存為一個常數
+const AUTHORIZATION_KEY = "<key/>";
+
+//針對某地區發送API請求
+const LOCATION_NAME = "臺北";
+
 //帶入APP元件使用
 function App() {
+  //使用useState並定義currentTheme預設值為light
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  //定義會使用到的資料狀態(參考API給的回應格式)
+  const [currentWeather, setCurrentWeather] = useState({
+    locationName: "臺北市",
+    description: "多雲時晴",
+    windSpeed: 1.1,
+    temperature: "22.9",
+    rainPossibility: 48.3,
+    observationTime: "2020-12-12 22:10:00",
+  });
+
+  //定義handleClick方法
+  const handleClick = () => {
+    fetch(
+      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // STEP 1：定義 `locationData` 把回傳的資料中會用到的部分取出來
+        const locationData = data.records.location[0];
+
+        // STEP 2：將風速（WDSD）和氣溫（TEMP）的資料取出
+        const weatherElements = locationData.weatherElement.reduce(
+          (neededElements, item) => {
+            if (["WDSD", "TEMP"].includes(item.elementName)) {
+              neededElements[item.elementName] = item.elementValue;
+            }
+            return neededElements;
+          },
+          {}
+        );
+
+        // STEP 3：要使用到 React 組件中的資料
+        setCurrentWeather({
+          observationTime: locationData.time.obsTime,
+          locationName: locationData.locationName,
+          temperature: weatherElements.TEMP,
+          windSpeed: weatherElements.WDSD,
+          description: "多雲時晴",
+          rainPossibility: 60,
+        });
+      });
+  };
   return (
-    <Container>
-      <WeatherCard>
-        <Location>臺北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23<Celsius>C</Celsius>
-          </Temperature>
-          <DayCloudy />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon />
-          23m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon />
-          48%
-        </Rain>
-        <Refresh>
-          最後觀測時間：上午12:03
-          <RefreshIcon />
-        </Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        <WeatherCard>
+          <Location>{currentWeather.locationName}</Location>
+          <Description>{currentWeather.description}</Description>
+          <CurrentWeather>
+            <Temperature>
+              {Math.round(currentWeather.temperature)}
+              <Celsius>C</Celsius>
+            </Temperature>
+            <DayCloudy />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon />
+            {currentWeather.windSpeed}
+          </AirFlow>
+          <Rain>
+            <RainIcon />
+            {currentWeather.rainPossibility}
+          </Rain>
+          {/* 綁定onClick時會呼叫handleClick方法*/}
+          <Refresh onClick={handleClick}>
+            最後觀測時間：
+            {new Intl.DateTimeFormat("zh-TW", {
+              hour: "numeric",
+              minute: "numeric",
+            }).format(new Date(currentWeather.observationTime))}{" "}
+            <RefreshIcon />
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 }
 
