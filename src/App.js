a6bfddaf084f69889150d17a30a83a84fbb2d114
niniku <niniku1457@gmail.com>
2024-10-01 22:23:14 +0800

    (update)real API form-change

--- src/App.js
+++ src/App.js
@@ -146,41 +146,42 @@ const theme = {
   },
 };
 //將自己的授權碼存為一個常數
-const AUTHORIZATION_KEY = "<key/>";
+const AUTHORIZATION_KEY = "CWA-F0C23CB4-9E98-4689-961C-76B38B358FA3";
 
 //針對某地區發送API請求
-const LOCATION_NAME = "臺北";
+const LOCATION_NAME = "臺中市";
 
 //第二隻API要帶入的值不同，另外宣告一個變數名稱
-const LOCATION_NAME_FORECAST = "臺北市";
+const LOCATION_NAME_FORECAST = "466920";
 
 //！因為fetchCurrentWeather及fetWeatherForecast不再需要使用setWeatherElement方法
 //所以可以搬到<App/>元件外自由使用
+
 const fetchCurrentWeather = () => {
   //加上return 直接回傳fetch API回傳的promise
   return fetch(
-    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`
+    `https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-F0C23CB4-9E98-4689-961C-76B38B358FA3&StationId=467490`
   )
     .then((response) => response.json())
     .then((data) => {
       // STEP 1：定義 `locationData` 把回傳的資料中會用到的部分取出來
-      const locationData = data.records.location[0];
+      const locationData = data.records.Station[0];
 
-      // STEP 2：將風速（WDSD）和氣溫（TEMP）的資料取出
-      const weatherElements = locationData.weatherElement.reduce(
-        (neededElements, item) => {
-          if (["WDSD", "TEMP"].includes(item.elementName)) {
-            neededElements[item.elementName] = item.elementValue;
-          }
-          return neededElements;
-        },
-        {}
-      );
+      // // STEP 2：將風速（WDSD）和氣溫（TEMP）的資料取出
+      // const weatherElements = locationData.weatherElement.reduce(
+      //   (neededElements, item) => {
+      //     if (["WDSD", "TEMP"].includes(item.elementName)) {
+      //       neededElements[item.elementName] = item.elementValue;
+      //     }
+      //     return neededElements;
+      //   },
+      //   {}
+      // );
       return {
-        observationTime: locationData.time.obsTime,
-        locationName: locationData.locationName,
-        temperature: weatherElements.TEMP,
-        windSpeed: weatherElements.WDSD,
+        observationTime: locationData.ObsTime.DateTime,
+        locationName: locationData.StationName,
+        temperature: locationData.WeatherElement.AirTemperature,
+        windSpeed: locationData.WeatherElement.WindSpeed,
       };
     });
 };
@@ -188,7 +189,7 @@ const fetchCurrentWeather = () => {
 //呼叫天氣預報API
 const fetchWeatherForecast = () => {
   return fetch(
-    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME_FORECAST}`
+    `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-F0C23CB4-9E98-4689-961C-76B38B358FA3&locationName=%E8%87%BA%E4%B8%AD%E5%B8%82`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -236,30 +237,30 @@ function App() {
 
   //加入useEffect方法
   //useEffect參數需要帶入一個函式，這個函式會在 畫面轉譯完成 後被呼叫
-  //放入fetchCurrentWeather方法(原本的handleClick)及dependencies([])
+
+  //把fetchData搬出useEffect
+  const fetchData = async () => {
+    //在抓取資料前，先把isLoading狀態改為true
+    setWeatherElement((prevState) => ({
+      ...prevState,
+      isLoading: true,
+    }));
+    //step2. 使用promise.all搭配await，等待兩支API都取得回應後才繼續
+    //直接透過陣列的解構賦值來取出Promise.all回傳的資料
+    const [currentWeather, weatherForecast] = await Promise.all([
+      fetchCurrentWeather(),
+      fetchWeatherForecast(),
+    ]);
+
+    //把取得的資料透過物件的解構賦值放入
+    setWeatherElement({
+      ...currentWeather,
+      ...weatherForecast,
+      isLoading: false,
+    });
+  };
+
   useEffect(() => {
-    //step1. 定義async Function fetchData()
-    const fetchData = async () => {
-      //在抓取資料前，先把isLoading狀態改為true
-      setWeatherElement((prevState) => ({
-        ...prevState,
-        isLoading: true,
-      }));
-      //step2. 使用promise.all搭配await，等待兩支API都取得回應後才繼續
-      //直接透過陣列的解構賦值來取出Promise.all回傳的資料
-      const [currentWeather, weatherForecast] = await Promise.all([
-        fetchCurrentWeather(),
-        fetchWeatherForecast(),
-      ]);
-
-      //把取得的資料透過物件的解構賦值放入
-      setWeatherElement({
-        ...currentWeather,
-        ...weatherForecast,
-        isLoading: false,
-      });
-    };
-
     //step3. 在useEffect中呼叫此fetchData()
     fetchData();
   }, []);
@@ -300,14 +301,8 @@ function App() {
             <RainIcon />
             {rainPossibility}
           </Rain>
-          {/* 更新onClick時會呼叫fetchCurrentWeather及fetchWeatherForecast方法*/}
-          <Refresh
-            onClick={() => {
-              fetchCurrentWeather();
-              fetchWeatherForecast();
-            }}
-            isLoading={isLoading}
-          >
+          {/* 更新onClick時會呼叫fetchData*/}
+          <Refresh onClick={fetchData} isLoading={isLoading}>
             最後觀測時間：
             {new Intl.DateTimeFormat("zh-TW", {
               hour: "numeric",
